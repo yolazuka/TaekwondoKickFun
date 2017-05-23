7ed689e0c4e798f3d4bab92b00d2d363a22d3e17
Yolazuka <yola.martin@gmail.com>
2017-05-23 21:53:42 +0200

    Update 23/05

--- app/src/main/java/com/example/usuario/taekwondokickfun/MainActivity.java
+++ app/src/main/java/com/example/usuario/taekwondokickfun/MainActivity.java
@@ -6,10 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import static com.example.usuario.taekwondokickfun.R.id.textomarcador2;
 
 public class MainActivity extends AppCompatActivity {
-
 
     private static final String SCORE_GIRL = "Score_Girl";
     private static final String SCORE_BOY = "Score_Boy";
@@ -17,22 +15,22 @@ public class MainActivity extends AppCompatActivity {
     int quantity = 0;
     int quantity2 = 0;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         MediaPlayer mediaPlayer;
-        mediaPlayer = MediaPlayer.create(this,R.raw.chinese);
+        mediaPlayer = MediaPlayer.create(this, R.raw.chinese);
         mediaPlayer.setLooping(true);
-        mediaPlayer.setVolume(100,100);
+        mediaPlayer.setVolume(100, 100);
         mediaPlayer.start();
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         // Save the current state
+
         savedInstanceState.putInt(SCORE_GIRL, quantity);
         savedInstanceState.putInt(SCORE_BOY, quantity2);
 
@@ -44,12 +42,14 @@ public class MainActivity extends AppCompatActivity {
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         // Always call the superclass so it can restore the view hierarchy
         super.onRestoreInstanceState(savedInstanceState);
+
         // Restore state members from saved instance
         quantity = savedInstanceState.getInt(SCORE_GIRL);
         quantity2 = savedInstanceState.getInt(SCORE_BOY);
+        display(quantity);
+        display2(quantity2);
 
     }
-
 
     /**
      * This method is called when the bodyattack button is clicked.
@@ -68,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
         quantity = quantity + 3;
         display(quantity);
-        String contadorMessage = "3 points more to your counter!!" +  "\nNice!";
+        String contadorMessage = "3 points more to your counter!!" + "\nNice!";
         displayMessage(contadorMessage);
     }
 
@@ -78,10 +78,9 @@ public class MainActivity extends AppCompatActivity {
 
         quantity = quantity + 3;
         display(quantity);
-        String contadorMessage = "You are doing it really good... 3 plus points" +  "\nSuper!";
+        String contadorMessage = "You are doing it really good... 3 plus points" + "\nSuper!";
         displayMessage(contadorMessage);
     }
-
 
     /* This method is called when the caution button is clicked.
   */
@@ -89,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
         quantity = quantity - 1;
         display(quantity);
-        String contadorMessage = "Oooouch ! that was painful... 1 point is going away :(" +  "\nBe careful!";
+        String contadorMessage = "Oooouch ! that was painful... 1 point is going away :(" + "\nBe careful!";
         displayMessage(contadorMessage);
     }
 
@@ -103,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
         marcador.setText(String.valueOf(score));
 
     }
+
     /**
      * This method displays the given quantity of points on the counter2
      */
@@ -128,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
 
         quantity2 = quantity2 + 3;
         display2(quantity2);
-        String contadorMessage2 = "You won 3 superpowerful points!!" +  "\nNice!";
+        String contadorMessage2 = "You won 3 superpowerful points!!" + "\nNice!";
         displayMessage2(contadorMessage2);
     }
 
@@ -138,10 +138,9 @@ public class MainActivity extends AppCompatActivity {
 
         quantity2 = quantity2 + 3;
         display2(quantity2);
-        String contadorMessage2 = "You are ace!... 3 plus points" +  "\nSuper!";
+        String contadorMessage2 = "You are ace!... 3 plus points" + "\nSuper!";
         displayMessage2(contadorMessage2);
     }
-
 
     /* This method is called when the caution2 button is clicked.
   */
@@ -149,7 +148,7 @@ public class MainActivity extends AppCompatActivity {
 
         quantity2 = quantity2 - 1;
         display2(quantity2);
-        String contadorMessage2 = "Oooouch ! that was too hard... 1 point less >_<" +  "\nWatch out!";
+        String contadorMessage2 = "Oooouch ! that was too hard... 1 point less >_<" + "\nWatch out!";
         displayMessage2(contadorMessage2);
     }
 
@@ -167,7 +166,6 @@ public class MainActivity extends AppCompatActivity {
         displayMessage2(contadorMessage2);
 
     }
-
 
     public void displayMessage2(String displayMessage2) {
         TextView textomarcador2 = (TextView) findViewById(R.id.textomarcador2);
